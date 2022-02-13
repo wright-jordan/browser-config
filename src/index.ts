3c87cba6a2c29df0fdc9143f121a03777cf5a07b
Jordan Wright <wright.jordan.n@outlook.com>
2022-02-12 20:24:35 -0600

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
 /// <reference lib="dom"/>
-
-const h1 = document.querySelector("h1");
-if (!(h1 instanceof HTMLHeadingElement)) {
-  throw new Error("h1 element not found");
-}
-
-h1.addEventListener("click", function (_ev) {
-  if (this.style.backgroundColor === "red") {
-    this.style.backgroundColor = "blue";
-    return;
-  }
-  this.style.backgroundColor = "red";
-});
-
-export {};
